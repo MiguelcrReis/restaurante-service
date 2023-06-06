d855d5acbed2b20932fbb44f82c8473aaf4cd152
Miguel Reis <miguel.reis@megalaudo.com.br>
2023-06-06 16:09:34 -0300

    Configured RabbitMqSubscriber in ItemService

--- ItemService/RabbitMqClient/RabbitMqSubscribrer.cs
+++ ItemService/RabbitMqClient/RabbitMqSubscribrer.cs
@@ -1,31 +1,46 @@
-﻿using RabbitMQ.Client;
+﻿using ItemService.EventProcessor;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
 
-namespace ItemService.RabbitMqClient
+namespace ItemService.RabbitMqClient;
+
+public class RabbitMqSubscribrer : BackgroundService
 {
-    public class RabbitMqSubscribrer :BackgroundService
+    private readonly IConfiguration _configuration;
+    private readonly string _nomeDaFila;
+    private readonly IConnection _connection;
+    private readonly IModel _channel;
+    private readonly IProcessaEvento _processaEvento;
+
+    public RabbitMqSubscribrer(IConfiguration configuration, IProcessaEvento processaEvento)
     {
-        private readonly IConfiguration _configuration;
-        private readonly string _nomeDaFila;
-        private readonly IConnection _connection;
-        private readonly IModel _channel;
-
-        public RabbitMqSubscribrer(IConfiguration configuration)
+        _configuration = configuration;
+        _connection = new ConnectionFactory()
         {
-            _configuration = configuration;
-            _connection = new ConnectionFactory()
-            {
-                HostName = "localhost",
-                Port = 8002
-            }.CreateConnection();
-            _channel = _connection.CreateModel();
-            _channel.ExchangeDeclare(exchange: "trigger", type: ExchangeType.Fanout);
-            _nomeDaFila = _channel.QueueDeclare().QueueName;
-            _channel.QueueBind(queue: _nomeDaFila, exchange: "trigger", routingKey: "");
-        }
+            HostName = _configuration["RabbitMqHost"],
+            Port = Int32.Parse(_configuration["RabbitMqPort"])
+        }.CreateConnection();
+        _channel = _connection.CreateModel();
+        _channel.ExchangeDeclare(exchange: "trigger", type: ExchangeType.Fanout);
+        _nomeDaFila = _channel.QueueDeclare().QueueName;
+        _channel.QueueBind(queue: _nomeDaFila, exchange: "trigger", routingKey: "");
+        _processaEvento = processaEvento;
+    }
 
-        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        EventingBasicConsumer? consumidor = new EventingBasicConsumer(_channel);
+
+        consumidor.Received += (ModuleHandle, ea) =>
         {
-            throw new NotImplementedException();
-        }
+            ReadOnlyMemory<byte> body = ea.Body;
+            string? mensagem = Encoding.UTF8.GetString(body.ToArray());
+            _processaEvento.Processa(mensagem);
+        };
+
+        // Informa que foi consumido a fila (autoAck) 
+        _channel.BasicConsume(queue: _nomeDaFila, autoAck: true, consumer: consumidor);
+        return Task.CompletedTask;
     }
 }
